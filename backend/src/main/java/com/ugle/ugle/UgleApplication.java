a163e89f5563210d41b9870cb5866ae2001f6b03
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-23 09:49:29 -0300

    Starting backend

---
+++ backend/src/main/java/com/ugle/ugle/UgleApplication.java
@@ -0,0 +1,13 @@
+package com.ugle.ugle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ugle/ugle/UgleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugle.ugle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
